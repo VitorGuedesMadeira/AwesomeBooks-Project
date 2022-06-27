33eb8c27ea231a572328268d8db5d84b8bd73174
Vitor Guedes <vgm_rox@hotmail.com>
2022-06-27 19:30:43 -0300

    Fix linters for javascript

--- js folder/index.js
+++ js folder/index.js
@@ -3,67 +3,63 @@
 const addBtn = document.querySelector('#addBtn');
 const title = document.querySelector('#titleId');
 const author = document.querySelector('#authorId');
-const bookUl = document.querySelector('#books')
-
+const bookUl = document.querySelector('#books');
 
 // ------------------------ DATA ----------------------------- //
 
-let booksData = []
+let booksData = [];
 
 // ------------------------ FUNCTIONS ------------------------ //
-function BookConstructor(title, author){
+function BookConstructor(title, author) {
   this.title = title;
   this.author = author;
 }
 
 function loadBooks(index) {
-  //creatingElements
+  // creatingElements
   const bookLi = document.createElement('li');
   const titleP = document.createElement('p');
   const authorP = document.createElement('p');
   const deleteBtn = document.createElement('button');
   deleteBtn.setAttribute('id', index);
   deleteBtn.setAttribute('class', 'buttons');
-  //content inside
+  // content inside
   titleP.textContent = booksData[index].title;
   authorP.textContent = booksData[index].author;
   deleteBtn.textContent = 'remove';
-  //appendChilds
+  // appendChilds
   bookLi.appendChild(titleP);
   bookLi.appendChild(authorP);
   bookLi.appendChild(deleteBtn);
   bookUl.appendChild(bookLi);
 
-  deleteBtn.addEventListener('click', (e) => {
+  deleteBtn.addEventListener('click', () => {
     booksData.splice(index, 1);
     bookUl.removeChild(bookLi);
     localStorage.setItem('books', JSON.stringify(booksData));
-  })
+  });
 }
 
 // ----------------------- EVENT LISTENER ------------------- //
 
 addBtn.addEventListener('click', () => {
-  let titleName = title.value;
-  let authorName = author.value;
+  const titleName = title.value;
+  const authorName = author.value;
   title.value = '';
   author.value = '';
-  let addNewBook = new BookConstructor(titleName, authorName);
+  const addNewBook = new BookConstructor(titleName, authorName);
   booksData.push(addNewBook);
-  loadBooks(booksData.length-1);
+  loadBooks(booksData.length - 1);
   localStorage.setItem('books', JSON.stringify(booksData));
-})
+});
 
-  // ---------------------- LOCAL STORAGE ---------------------- //
+// ---------------------- LOCAL STORAGE ---------------------- //
 
-  window.addEventListener('load', () => {
-    if (localStorage.getItem('books')) {
-      booksData = JSON.parse(localStorage.getItem('books'));
-    }
-    for(let i = 0; i < booksData.length; i++) {
-      loadBooks(i);
-    }
-  })
-  
-  
- 
+window.addEventListener('load', () => {
+  if (localStorage.getItem('books')) {
+    booksData = JSON.parse(localStorage.getItem('books'));
+  }
+  for (let i = 0; i < booksData.length; i += 1) {
+    loadBooks(i);
+  }
+});
