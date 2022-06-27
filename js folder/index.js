16cd2ca2519feebed94d816a0d8d9ebf430b9a92
Vitor Guedes <vgm_rox@hotmail.com>
2022-06-27 18:27:10 -0300

    Add remove btns for books section

--- js folder/index.js
+++ js folder/index.js
@@ -1,3 +1,19 @@
+// ---------------------- VARIABLES -------------------------- //
+const addBtn = document.querySelector('#addBtn');
+const title = document.querySelector('#titleId');
+const author = document.querySelector('#authorId');
+const bookUl = document.querySelector('#books')
+
+// ---------------------- LOCAL STORAGE ---------------------- //
+
+bookUl.addEventListener('input', () => {
+  localStorage.setItem('title', `${title.value}`);
+  localStorage.setItem('author', `${author.value}`);
+});
+
+title.value = localStorage.getItem('nameValue');
+author.value = localStorage.getItem('emailValue');
+
 // ------------------------ DATA ----------------------------- //
 const booksData = [
 {
@@ -10,33 +26,37 @@ const booksData = [
 }
 ]
 
-const addBtn = document.querySelector('#addBtn');
-const title = document.querySelector('#titleId');
-const author = document.querySelector('#authorId');
-const bookUl = document.querySelector('#books')
-
+// ------------------------ FUNCTIONS ------------------------ //
 function BookConstructor(title, author){
   this.title = title;
   this.author = author;
 }
 
 function loadBooks(index) {
+  //creatingElements
   const bookLi = document.createElement('li');
   const titleP = document.createElement('p');
   const authorP = document.createElement('p');
   const deleteBtn = document.createElement('button');
-
+  deleteBtn.setAttribute('id', index);
+  deleteBtn.setAttribute('class', 'buttons');
+  //content inside
   titleP.textContent = booksData[index].title;
   authorP.textContent = booksData[index].author;
   deleteBtn.textContent = 'remove';
-
+  //appendChilds
   bookLi.appendChild(titleP);
   bookLi.appendChild(authorP);
   bookLi.appendChild(deleteBtn);
   bookUl.appendChild(bookLi);
+
+  deleteBtn.addEventListener('click', (e) => {
+    booksData.splice(index, 1);
+    bookUl.removeChild(bookLi);
+  })
 }
 
-// ----------------------- event listener ------------------- //
+// ----------------------- EVENT LISTENER ------------------- //
 
 addBtn.addEventListener('click', () => {
   let titleName = title.value;
@@ -45,10 +65,9 @@ addBtn.addEventListener('click', () => {
   author.value = '';
   let addNewBook = new BookConstructor(titleName, authorName);
   booksData.push(addNewBook);
-
-  loadBooks(booksData.length - 1)
+  loadBooks(booksData.length-1);
 })
 
-for (let i = 0; i < booksData.length; i++) {
+for(let i = 0; i < booksData.length; i++) {
   loadBooks(i)
 }
