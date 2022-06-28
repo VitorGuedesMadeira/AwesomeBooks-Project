6aa39752a5d4b08b334a79312c94d21dbbfa7816
Vitor Guedes <vgm_rox@hotmail.com>
2022-06-28 11:44:30 -0300

    Replace functions for classes

--- js/index.js
+++ js/index.js
@@ -1,47 +1,61 @@
 // ---------------------- VARIABLES -------------------------- //
-
 const addBtn = document.querySelector('#addBtn');
 const title = document.querySelector('#titleId');
 const author = document.querySelector('#authorId');
 const bookUl = document.querySelector('#books');
 
 // ------------------------ DATA ----------------------------- //
-
 let booksData = [];
 
 // ------------------------ FUNCTIONS ------------------------ //
-function BookConstructor(title, author) {
+/* function BookConstructor(title, author) {
   this.title = title;
   this.author = author;
+} */
+
+class BookConstructor {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 }
 
-function loadBooks(index) {
-  // creatingElements
-  const bookLi = document.createElement('li');
-  const titleP = document.createElement('p');
-  const authorP = document.createElement('p');
-  const deleteBtn = document.createElement('button');
-  deleteBtn.setAttribute('id', index);
-  deleteBtn.setAttribute('class', 'buttons');
-  // content inside
-  titleP.textContent = booksData[index].title;
-  authorP.textContent = booksData[index].author;
-  deleteBtn.textContent = 'remove';
-  // appendChilds
-  bookLi.appendChild(titleP);
-  bookLi.appendChild(authorP);
-  bookLi.appendChild(deleteBtn);
-  bookUl.appendChild(bookLi);
-
-  deleteBtn.addEventListener('click', () => {
-    booksData.splice(index, 1);
-    bookUl.removeChild(bookLi);
-    localStorage.setItem('books', JSON.stringify(booksData));
-  });
+class Dynamic {
+  static loadBooks(index) {
+    // creatingElements
+    const bookLi = document.createElement('li');
+    const titleP = document.createElement('p');
+    const authorP = document.createElement('p');
+    const deleteBtn = document.createElement('button');
+    // content inside
+    titleP.textContent = `"${booksData[index].title}"`;
+    authorP.textContent = `by ${booksData[index].author}`;
+    deleteBtn.textContent = 'Remove';
+    // div for title and author
+    const divTitleAndAuthor = document.createElement('div');
+    // appendChilds
+    divTitleAndAuthor.appendChild(titleP);
+    divTitleAndAuthor.appendChild(authorP);
+    bookLi.appendChild(divTitleAndAuthor);
+    bookLi.appendChild(deleteBtn);
+    bookUl.appendChild(bookLi);
+    // giving classes
+    bookLi.setAttribute('class', 'liStyles')
+    deleteBtn.setAttribute('id', index);
+    deleteBtn.setAttribute('class', 'buttons');
+    divTitleAndAuthor.setAttribute('class', 'displayFlex');
+    if(index%2==1){
+      bookLi.setAttribute('class', 'liStyles backgroundTransparent')
+    }
+    deleteBtn.addEventListener('click', () => {
+      booksData.splice(index, 1);
+      bookUl.removeChild(bookLi);
+      localStorage.setItem('books', JSON.stringify(booksData));
+    });
+  }
 }
 
 // ----------------------- EVENT LISTENER ------------------- //
-
 addBtn.addEventListener('click', () => {
   const titleName = title.value;
   const authorName = author.value;
@@ -49,17 +63,17 @@ addBtn.addEventListener('click', () => {
   author.value = '';
   const addNewBook = new BookConstructor(titleName, authorName);
   booksData.push(addNewBook);
-  loadBooks(booksData.length - 1);
+  Dynamic.loadBooks(booksData.length - 1);
   localStorage.setItem('books', JSON.stringify(booksData));
 });
 
-// ---------------------- LOCAL STORAGE ---------------------- //
 
+// ---------------------- LOCAL STORAGE ---------------------- //
 window.addEventListener('load', () => {
   if (localStorage.getItem('books')) {
     booksData = JSON.parse(localStorage.getItem('books'));
   }
   for (let i = 0; i < booksData.length; i += 1) {
-    loadBooks(i);
+    Dynamic.loadBooks(i);
   }
 });
