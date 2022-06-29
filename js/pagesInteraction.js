d4c468700bb48cd30e2a780216164f8da2756c40
Vitor Guedes <vgm_rox@hotmail.com>
2022-06-29 00:20:52 -0300

    Fix linters

--- js/pagesInteraction.js
+++ js/pagesInteraction.js
@@ -6,24 +6,24 @@ const booksSection = document.querySelector('#booksCollection');
 const newBookSection = document.querySelector('#newBookSection');
 const contactForm = document.querySelector('#contactForm'); 
 
-navAList.forEach(child => {
-  if(child.classList.contains('list')) {
+navAList.forEach((child) => {
+  if (child.classList.contains('list')) {
     child.addEventListener('click', () => {
       booksSection.classList.remove('hide');
       newBookSection.classList.add('hide');
       contactForm.classList.add('hide');
     });
-    }else if(child.classList.contains('newBook')){
-      child.addEventListener('click', () => {
-        newBookSection.classList.remove('hide');
-        booksSection.classList.add('hide');
-        contactForm.classList.add('hide');
-      });
-    }else {
-      child.addEventListener('click', () => {
-        contactForm.classList.remove('hide');
-        newBookSection.classList.add('hide');
-        booksSection.classList.add('hide');
-      });
-    }
-  });
+  } else if (child.classList.contains('newBook')) {
+    child.addEventListener('click', () => {
+      newBookSection.classList.remove('hide');
+      booksSection.classList.add('hide');
+      contactForm.classList.add('hide');
+    });
+  } else {
+    child.addEventListener('click', () => {
+      contactForm.classList.remove('hide');
+      newBookSection.classList.add('hide');
+      booksSection.classList.add('hide');
+    });
+  }
+});
